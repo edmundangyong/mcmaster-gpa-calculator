4a12f543d6e28978154cc4a83394e8e735cf7115
edmundangyong <160616980+edmundangyong@users.noreply.github.com>
2026-05-27 23:57:02 -0400

    text to dict extraction

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import fitz
 import easyocr
+import re
 
 def extract_pdf(pdf_path, lang='en'):
     print("EXTRACTING PDF")
@@ -23,13 +24,44 @@ def extract_pdf(pdf_path, lang='en'):
 
     doc.close()
     print("PDF EXTRACTION COMPLETE")
-    return(full_document_text)
+    return("\n\n".join(full_document_text))
+
+def extract_dict(transcript):
+    print("EXTRACTING DICTIONARY")
+
+    transcript = transcript.split("\n")
+
+    grades = {}
+    pattern1 = r"^\d\.\d{2}\/\d\.\d{2}$"
+
+    for line in range(len(transcript)):
+
+        clean_line = transcript[line].strip()
+
+        if re.match(pattern1, clean_line):
+
+            course_code = transcript[line-2]
+            course_name = transcript[line-1]
+            weight = clean_line
+            grade = transcript[line+1]
+
+            grades[course_code] = {
+                "name": course_name,
+                "weight": weight,
+                "grade": grade
+            }
+
+            print(course_code, course_name, weight, grade)
+    
+    print("DICTIONARY EXTRACTION COMPLETE")
+    return grades
 
 if __name__ == "__main__":
     pdf_file = "test.pdf" 
-    extracted_text = "\n\n".join(extract_pdf(pdf_file))
+    extracted_text = extract_pdf(pdf_file)
     
     with open("output.txt", "w", encoding="utf-8") as text_file:
         text_file.write(extracted_text)
     
+    extract_dict(extracted_text)
     print("DONE")
